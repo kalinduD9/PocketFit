ff3e2412259e5e8cc412be2b2fc281118f94b3f6
Kalindu Dulshan <kalindudw9@gmail.com>
2026-01-30 22:47:22 +0530

    Create project package structure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.compose.material:material-icons-extended:1.6.2")
+    implementation("androidx.compose.material3:material3-window-size-class:1.2.1")
 }
